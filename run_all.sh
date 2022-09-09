5e1785733941799c81c16e3b6d9d2e03973dd87c
Dhananjayadmd <dmdwijerathne@gmail.com>
2022-09-09 12:36:13 +0800

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,12 @@
-python run_morpher.py morpher_benchmarks/array_add/array_add.c array_add
-python run_morpher.py morpher_benchmarks/hpcg/hpcg.c hpcg
-python run_morpher.py morpher_benchmarks/trmm/trmm.c trmm
-python run_morpher.py pace_benchmarks/gemm/gemm_no_unroll_flattened/gemm.c gemm
-python run_morpher.py pace_benchmarks/conv2d/no_unroll_2x2_filt/convolution2d.c convolution2d
-python run_morpher.py pace_benchmarks/conv2d/no_unroll_3x3_filt/convolution2d.c convolution2d
-python run_morpher.py opencgra_benchmarks/fir/kernel.c kernel
-python run_morpher.py cgrame_microbench/sum/sum.c main
-python run_morpher.py cgrame_microbench/mac/mac.c main
-python run_morpher.py cgrame_microbench/mac2/mac2.c main
-python run_morpher.py cgrame_microbench/conv2/conv2.c main
-python run_morpher.py cgrame_microbench/conv3/conv3.c main
+python -u run_morpher.py morpher_benchmarks/array_add/array_add.c array_add
+python -u run_morpher.py morpher_benchmarks/hpcg/hpcg.c hpcg
+python -u run_morpher.py morpher_benchmarks/trmm/trmm.c trmm
+python -u run_morpher.py pace_benchmarks/gemm/gemm_no_unroll_flattened/gemm.c gemm
+python -u run_morpher.py pace_benchmarks/conv2d/no_unroll_2x2_filt/convolution2d.c convolution2d
+python -u run_morpher.py pace_benchmarks/conv2d/no_unroll_3x3_filt/convolution2d.c convolution2d
+python -u run_morpher.py opencgra_benchmarks/fir/kernel.c kernel
+python -u run_morpher.py cgrame_microbench/sum/sum.c main
+python -u run_morpher.py cgrame_microbench/mac/mac.c main
+python -u run_morpher.py cgrame_microbench/mac2/mac2.c main
+python -u run_morpher.py cgrame_microbench/conv2/conv2.c main
+python -u run_morpher.py cgrame_microbench/conv3/conv3.c main
