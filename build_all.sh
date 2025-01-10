7ed3886c68ab03b4df30155714b3c945e936cbf0
Zhaoying Li <lizhaoyinglzy@gmail.com>
2025-01-10 14:10:58 +0800

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,17 +1,17 @@
 cd Morpher_DFG_Generator
 mkdir build
 cd build
-cmake   ..
+cmake ..
 make all -j 2
 cd ../../Morpher_CGRA_Mapper
 mkdir build
 cd build
-cmake   -DCMAKE_BUILD_TYPE=Release ..
-make all -j 8
+cmake ..
+make all -j
 cd ../../hycube_simulator
 cd src
 mkdir build
 cd build
-cmake  ..
-make all -j 8
+cmake ..
+make all -j
 echo "buld success!!!"
